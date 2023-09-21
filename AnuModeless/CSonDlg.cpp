32c8e0ebb7c979c76c39c046e5309bc6f986a2c5
MetoZec <87604547+Simonsbed@users.noreply.github.com>
2023-09-21 21:35:40 +0900

    초기 커밋.

---
+++ AnuModeless/CSonDlg.cpp
@@ -0,0 +1,64 @@
+﻿// CSonDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "AnuModeless.h"
+#include "afxdialogex.h"
+#include "CSonDlg.h"
+
+
+// CSonDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CSonDlg, CDialogEx)
+
+CSonDlg::CSonDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DLG_MODELESS, pParent)
+	, m_Str(_T(""))
+{
+
+}
+
+CSonDlg::~CSonDlg()
+{
+}
+
+void CSonDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_Str);
+}
+
+
+BEGIN_MESSAGE_MAP(CSonDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSonDlg::OnBnClickedButton1)
+//	ON_WM_CLOSE()
+ON_WM_CLOSE()
+END_MESSAGE_MAP()
+
+
+// CSonDlg 메시지 처리기
+
+#include "AnuModelessDlg.h"
+void CSonDlg::OnBnClickedButton1()
+{
+	CAnuModelessDlg* p = (CAnuModelessDlg*)GetParent();
+	UpdateData(true);
+	p->m_List.AddString(m_Str);
+}
+
+
+void CSonDlg::OnClose()
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	DestroyWindow();
+
+	CDialogEx::OnClose();
+}
+
+
+void CSonDlg::PostNcDestroy()
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	CDialogEx::PostNcDestroy();
+}
---
+++ AnuModeless/CSonDlg.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CSonDlg 대화 상자
+
+class CSonDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CSonDlg)
+
+public:
+	CSonDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CSonDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_MODELESS };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	CString m_Str;
+	afx_msg void OnClose();
+	virtual void PostNcDestroy();
+};
